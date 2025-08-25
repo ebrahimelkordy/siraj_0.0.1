256b3403087c7524644ae96939b2316a58264230
Ebrahim ElKordy <ebrahimelkordy@example.com>
2025-08-25 16:13:12 +0300

    نشوف بحيري هيغمل ايه

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: {
+    host: "::",
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
